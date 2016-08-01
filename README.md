6f0305d77a0b551127a84bf9b62972c1e4977003
mizutombo <thomas.shultz52@gmail.com>
2016-08-01 12:57:16 -0700

    revised CSS code to enable styling and layout ... revised JS code to enable dynamic build of table to track image vote counts ... working on JS code to enable vote counting and tracking ... working on JS code to enable reload of webpage after each user vote ... updated README

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 * 7/27/2016 // initiate README for market-analysis-app ... created branch class10 ... ACP to branch class10 <br/>
 * 7/27/2016 // set up scaffolding directories and files per assignment setup instructions ... add images into img/ directory ... ACP to class10 <br/>
 * 7/30/2016 // ACP current code revs in JS and HTML files ... ACP to class10 <br/>
-* 
+* 7/31/2016 // completed CSS code for styling & layout ... completed JS code to dynamically build table ... worked on JS code to enable vote counting & tabulation ... not functioning as expected ... fails to reload webpage after each vote selection & fails to track vote count for each image <br/>
+* 8/1/2016 // continuing work on JS code to enable vote counting & tabulation ... continuing to fail in reloading webpage after each vote selection & still fails to track vote count for each image ... ACP current status <br/>
+*
--- js/app.js
+++ js/app.js
@@ -1,19 +1,18 @@
 
-  var totalClicks = 0;
-  var source = 0;
-  var name = 0;
-
-  var imgTrx = function() {
-    this.imageSource = source;
+  // create constructor object
+  var imgTrx = function(name, source) {
+    this.imgSource = source;
     this.upVotes = 0;
     this.name = name;
-  }
-  var imageOptions = [
+  };
+  var totalUpVotes = 0; // initiate var 'totalUpVotes' to track total upVotes
+  // create array of image & source sets
+  var imgOptions = [
     new imgTrx('Bag', 'img/bag.jpg'),
     new imgTrx('Banana', 'img/banana.jpg'),
     new imgTrx('Boots', 'img/boots.jpg'),
     new imgTrx('Chair', 'img/chair.jpg'),
-    new imgTrx('Cthulu', 'img/cthulu.jpg'),
+    new imgTrx('Cthulhu', 'img/cthulhu.jpg'),
     new imgTrx('Dragon', 'img/dragon.jpg'),
     new imgTrx('Pen', 'img/pen.jpg'),
     new imgTrx('Scissors', 'img/scissors.jpg'),
@@ -24,30 +23,57 @@
     new imgTrx('Water_Can', 'img/water_can.jpg'),
     new imgTrx('Wine_Glass', 'img/wine_glass.jpg'),
   ];
-    var pickedImages = []; // this is engine memory - tracks images that have been shown
-    document.getElementById('pix1').addEventListener('click', recordClick);
-    document.getElementById('pix2').addEventListener('click', recordClick);
-    document.getElementById('pix3').addEventListener('click', recordClick);
-    function getThreeImages() {
-      pickedImages = []; // empty this so that we can track 3 new images
-      for (var imageID = 1; imageID <= 3; imageID++) {
-        do { // gets a random index value for our image
-          var index = Math.floor(Math.random() * 14);
-        } while (pickedImages.indexOf(index) >= 0); // keep trying until unique
-        var source = imageOptions[index].imageSource; // get source for image
-        document.getElementById('pix'+imageID).src = source; // update image with new source
-        pickedImages.push(index); // add image location to engine memory for later usage
+  var pickedImages = []; // this is memory array; tracks images that have been shown to the user
+  document.getElementById('image-container').addEventListener('click', recordClick); // event listener to record clicks on selected images; triggers recordClick function
+  function getThreeImages() { // function to present 3 images to user for review and voting
+    pickedImages = []; // empty this memory array so that we can track 3 new images
+    for (var imageID = 1; imageID <= 3; imageID++) {
+      do { // gets a random index value for our image
+        var index = Math.floor(Math.random() * 14); // random number engine
       }
+      while (pickedImages.indexOf(index) >= 0); // method 'indexOf' returns index number of first time a character appears in string; keep cycling through random number engine until set of 3 unique images can be presented to user
+      var source = imgOptions[index].imgSource; // pull img source for image
+      document.getElementById('pix'+imageID).src = source; // update image presentation box with new image source to display image
+      pickedImages.push(index); // add image location to memory array for later usage
     }
-  function recordClick(event) {
+  };
+
+  function recordClick(event) { // image clicks recorder function; triggered by event listener
     var clickedImage = event.target;
     console.log(clickedImage);
     var clickedImageSource = clickedImage.src;
     console.log('clicked SRC: '+clickedImageSource);
-    for (var index = 0; index < imageOptions.length; index++) {
-      if (imageOptions[index].imageSource == clickedImageSource) {
-        imgTrx[index].upVotes++;
-        console.log('clicked item: '+imageOptions[index].name);
+    for (var index = 0; index < imgOptions.length; index++) {
+      if (clickedImageSource.indexOf(imgOptions[index].imgSource) >= 0) { // method 'indexOf' returns index number of first time a character appears in string;
+        imgOptions[index].upVotes++; // log up-vote for user-selected image
+        console.log('clicked item: '+imgOptions[index].name);
+        console.log(imgOptions[index]);
+        totalUpVotes += imgOptions[index].upVotes;
+        console.log(totalUpVotes);
+        document.getElementById('progress').innerHTML = 'You have completed '+totalUpVotes+' of 15 votes in this survey.' // Provide survey progress message to user
       }
     }
-  }
+  };
+  // do {
+    getThreeImages(); // call function to randomly render 3 unique images into presentation box for user to view for selection and voting
+  // }
+    // while (totalUpVotes <= 15) // *** CREATES FATAL LOOP !!!
+
+    // dynamically build table with product images and total up-votes
+    var table = document.getElementById('product-votes');
+    for (var index = 0; index < imgOptions.length; index++) {
+      // establish variable to enable run through full array of images
+      var currentImage = imgOptions[index];
+      var upVotes = 0;
+      var imageRow = document.createElement('tr');
+      var imageNameCell = document.createElement('td');
+      imageNameCell.innerText = currentImage.name;
+      imageRow.appendChild(imageNameCell);
+      // create row data cell for total up-votes for product image
+      var totalUpVotesCell = document.createElement('td');
+      totalUpVotesCell.innerText = currentImage.upVotes;
+      console.log(totalUpVotesCell.innerText);
+      imageRow.appendChild(totalUpVotesCell);
+      // append new row onto table
+      table.appendChild(imageRow);
+    };
